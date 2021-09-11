c248daaa27e5f3dc8940b96f2a1c96ff4dc467ac
MR.SHAGGY <87988393+mrSHAGGYboy@users.noreply.github.com>
2021-09-12 00:20:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,15 +22,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("BQACqD33kVKRS-5etL2BOyKO3F5xqTz2AhiNqDGR1Lrv6sddw3wfJzQBJ_mKpsXD--Y7y6jnglS9UDdYnV622dzIvpPN2XgYYWDXEorF0Gk6cWHJ_RHrLd6X2HWrd7BukbyN121L4q0EhrgdsevzUemx__SZbOJ8-r_Td6YkJ_MJR5skt4eBzdHUU0KzOdik0AKcHfEcvnaFyruzlIpUGiCn68WaWa14tMwivAlgwexWQSVcLWMpXGMQn2cyE8FgpJrXlaF_hu-XdAjbv26XV5ldNMyfahaQGylI672vJT44VdTkjOvA38jDid8pPoBmCyl6z4gSpynDW2C7YxqRo0g0YhViYgA", "session")
-BOT_TOKEN = getenv("1955724129:AAHn8D2Uu8-lu72onLEY44GNiPD_JfMoVu8")
-BOT_NAME = getenv("Music player")
+SESSION_NAME = getenv("", "session")
+BOT_TOKEN = getenv("")
+BOT_NAME = getenv("")
 admins = {}
-API_ID = int(getenv("6065291"))
-API_HASH = getenv("dc7873c0a5c737af4356d4f245fe696d")
+API_ID = int(getenv(""))
+API_HASH = getenv("")
 
 DURATION_LIMIT = int(getenv("7", "7"))
 
 COMMAND_PREFIXES = list(getenv("/", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("1153898821").split()))
+SUDO_USERS = list(map(int, getenv("").split()))
